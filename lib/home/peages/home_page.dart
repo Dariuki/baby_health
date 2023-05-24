70c295dd5fff40d627bdb4065b1d7aa2fb62666a
Dariuki <skwiera1@onet.eu>
2023-05-24 22:05:22 +0200

    Add PersonTile to HomePage

---
+++ lib/home/peages/home_page.dart
@@ -0,0 +1,36 @@
+import 'package:baby_health/home/widgets/person_tile.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Home Page'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: const _HomePageBody(),
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  const _HomePageBody();
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: const [
+        PersonTile(),
+        PersonTile(),
+        PersonTile(),
+      ],
+    );
+  }
+}
---
+++ lib/home/widgets/person_tile.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class PersonTile extends StatelessWidget {
+  const PersonTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 15, right: 15, top: 15),
+      child: Container(
+        padding: const EdgeInsets.all(8),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          color: Colors.amber,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              spreadRadius: 5,
+              blurRadius: 7,
+              offset: const Offset(4, 8),
+            ),
+          ],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            const CircleAvatar(
+              radius: 40,
+            ),
+            Row(
+              children: [
+                const Text(
+                  'Imie i nazwisko',
+                  style: TextStyle(fontSize: 20),
+                ),
+                const SizedBox(width: 10),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Edytuj'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
